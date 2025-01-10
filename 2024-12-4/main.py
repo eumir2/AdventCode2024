440fd9ff48c3d7f9aaa94b238be2352a56f2879f
Eumir Cometti <eumircometti.ec.ec@gmail.com>
2025-01-10 16:55:43 +0100

    Parte due flash

--- 2024-12-4/main.py
+++ 2024-12-4/main.py
@@ -58,6 +58,52 @@ def checkParola(matrix, r,c,direzione, parola):
             return False
         
     return True
+
+def parteDue():
+    f = open("input.txt")
     
     
-parteUno()
+    # Lettura da file
+    with open("input.txt") as f:
+        data = [list(line.strip()) for line in f]
+        
+    #print(data)
+    matrix = np.array(data)
+    
+    counter = 0
+    
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if matrix[i,j] == 'A':
+                if checkCroce(matrix, i,j,):
+                    counter += 1
+
+    print(counter)
+    
+def checkCroce(matrix, r, c):
+    n,m = matrix.shape
+    d_m_dx = r - 1, c + 1
+    d_m_sx = r + 1, c - 1
+    
+    d_M_dx = r + 1, c + 1
+    d_M_sx = r -1, c - 1 
+    
+    if indici_validi(n, m, *d_m_dx) and indici_validi(n, m, *d_m_sx) and indici_validi(n, m, *d_M_dx) and indici_validi(n, m, *d_M_sx):
+        tmp1 = matrix[d_M_sx[0],d_M_sx[1]]+matrix[r,c]+matrix[d_M_dx[0],d_M_dx[1]]
+    
+        tmp2 = matrix[d_m_sx[0],d_m_sx[1]]+matrix[r,c]+matrix[d_m_dx[0],d_m_dx[1]]
+
+        if sorted(tmp1) == sorted(tmp2) == sorted("MAS"):
+            return True
+        else:
+            return False
+    
+    return False
+ 
+def indici_validi(n, m, r, c):
+    return 0 <= r < n and 0 <= c < m   
+    
+    
+#parteUno()
+
+parteDue()
